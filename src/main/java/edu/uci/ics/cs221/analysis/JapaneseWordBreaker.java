faaf0a0c2729fc47fbac74ae65b57294dc625464
Ned <46288336+Nedhoven@users.noreply.github.com>
2019-04-16 23:10:27 -0700

    Update JapaneseWordBreaker.java

--- src/main/java/edu/uci/ics/cs221/analysis/JapaneseWordBreaker.java
+++ src/main/java/edu/uci/ics/cs221/analysis/JapaneseWordBreaker.java
@@ -6,43 +6,41 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class JapaneseWordBreaker implements Tokenizer {
-    Map<String, Double> wordDict = new HashMap<>(); // a map to store the word dictionary, key is the word, value is the frequency
-    List<String> dictLines;
+
+    Map<String, Double> dictionary;
 
     public JapaneseWordBreaker() {
         try {
-            URL dictResource = WordBreakTokenizer.class.getClassLoader().getResource("cs221_frequency_dictionary_jp.txt");
-            dictLines = Files.readAllLines(Paths.get(dictResource.toURI()));
-
+            dictionary = new HashMap<>();
+            String corpus = "cs221_frequency_dictionary_jp.txt";
+            URL dictResource = WordBreakTokenizer.class.getClassLoader().getResource(corpus);
+            List<String> dictLines = Files.readAllLines(Paths.get(dictResource.toURI()));
             double freqSum = 0;
             for (int i = 0; i < dictLines.size(); i++) {
                 freqSum += Double.parseDouble(dictLines.get(i).split(" ")[1]);
             }
-
             for (int i = 0; i < dictLines.size(); i++) {
                 String s = dictLines.get(i);
                 if (s.startsWith("\uFEFF")) {
                     s = s.substring(1);
                 }
-                this.wordDict.put(s.split(" ")[2], (double) Double.parseDouble(s.split(" ")[1]) / freqSum);
+                dictionary.put(s.split(" ")[2], (double) Double.parseDouble(s.split(" ")[1]) / freqSum);
             }
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     public List<String> tokenize(String text) {
-        List<String> possibleString = wordBreakTopDown(text.toLowerCase(), wordDict.keySet());
-
+        Set<String> keys = dictionary.keySet();
+        List<String> possibleString = wordBreakTopDown(text.toLowerCase(), keys);
         if (possibleString.size() == 0) {
-            throw new UnsupportedOperationException("there's no possible way to break the string!");
+            throw new UnsupportedOperationException("WordBreakerTokenizer Error: Unable to break the text!");
         }
-
-        double maxFreq = -Double.MAX_VALUE;
+        double maxFreq = -1 * Double.MAX_VALUE;
         String result = "";
         for (int i = 0; i < possibleString.size(); i++) {
-            double freq = computeFrequency(possibleString.get(i), wordDict);
+            double freq = computeFrequency(possibleString.get(i));
             if (freq > maxFreq) {
                 result = possibleString.get(i);
                 maxFreq = freq;
@@ -65,11 +63,9 @@ public class JapaneseWordBreaker implements Tokenizer {
         if (start == s.length()) {
             return Collections.singletonList("");
         }
-
         if (dp.containsKey(start)) {
             return dp.get(start);
         }
-
         List<String> words = new ArrayList<>();
         for (int i = start; i < start + max && i < s.length(); i++) {
             String newWord = s.substring(start, i + 1);
@@ -86,14 +82,14 @@ public class JapaneseWordBreaker implements Tokenizer {
         return words;
     }
 
-    private double computeFrequency(String s, Map<String, Double> dict) {
+    private double computeFrequency(String s) {
         StringTokenizer st = new StringTokenizer(s, " \t\n,.;?!");
         double logFreq = 0.0;
         while (st.hasMoreTokens()) {
             String temp = st.nextToken();
-            logFreq += Math.log(dict.get(temp));
+            logFreq += Math.log(dictionary.get(temp));
         }
         return logFreq;
     }
-    
+
 }
