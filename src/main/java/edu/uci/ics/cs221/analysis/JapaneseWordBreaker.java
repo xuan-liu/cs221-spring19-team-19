3bf9a707f917aa925e1b0842cbeb089d7a00aa65
Ned <46288336+Nedhoven@users.noreply.github.com>
2019-04-17 21:27:30 -0700

    Update JapaneseWordBreaker.java
    Containing Japanese stop words (common words) elimination

--- src/main/java/edu/uci/ics/cs221/analysis/JapaneseWordBreaker.java
+++ src/main/java/edu/uci/ics/cs221/analysis/JapaneseWordBreaker.java
@@ -26,7 +26,8 @@ public class JapaneseWordBreaker implements Tokenizer {
                 }
                 dictionary.put(s.split(" ")[2], (double) Double.parseDouble(s.split(" ")[1]) / freqSum);
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -35,30 +36,26 @@ public class JapaneseWordBreaker implements Tokenizer {
         if (text.length() == 0) {
             return Arrays.asList();
         }
-        return breakWordDP(text.toLowerCase(), dictionary);
+        return breakWordDP(text);
     }
 
-    private List<String> breakWordDP(String word, Map<String, Double> dict) {
+    private List<String> breakWordDP(String word) {
         int path[][] = new int[word.length()][word.length()];
         double logFreq[][] = new double[word.length()][word.length()];
-
         for (int i = 0; i < path.length; i++) {
             for (int j = 0; j < path[i].length ; j++) {
                 path[i][j] = -1;
                 logFreq[i][j] = -Double.MAX_VALUE;
             }
         }
-
         for (int l = 1; l <= word.length(); l++) {
             for (int i = 0; i < word.length() - l + 1 ; i++) {
                 int j = i + l-1;
                 String str = word.substring(i,j+1);
-
-                if (dict.containsKey(str)) {
-                    logFreq[i][j] = Math.log(dict.get(str));
+                if (dictionary.containsKey(str)) {
+                    logFreq[i][j] = Math.log(dictionary.get(str));
                     path[i][j] = i;
                 }
-
                 for(int k = i + 1; k <= j; k++){
                     if(path[i][k-1] != -1 && path[k][j] != -1){
                         if (logFreq[i][k-1] + logFreq[k][j] > logFreq[i][j]) {
@@ -69,24 +66,30 @@ public class JapaneseWordBreaker implements Tokenizer {
                 }
             }
         }
-
-        if(path[0][word.length()-1] == -1){
-            throw new RuntimeException("there's no possible way to break the string!");
+        if (path[0][word.length()-1] == -1){
+            throw new RuntimeException("WordBreakerTokenizer Error: unable to break the text!");
         }
-
         String result = backwardResult(path, word, 0, word.length() -1);
         PunctuationTokenizer pt = new PunctuationTokenizer();
-        return pt.tokenize(result);
+        List<String> ans = pt.tokenize(result);
+        for (int l = 0; l < ans.size(); l++) {
+            String temp = ans.get(l);
+            if (StopWords.stopWordsJapanese.contains(temp)) {
+                ans.remove(l);
+                l--;
+            }
+        }
+        return ans;
     }
 
-    private String backwardResult(int [][] path, String s, int i, int j) {
+    private String backwardResult(int[][] path, String s, int i, int j) {
         int k = path[i][j];
         if (i == k) {
             return s.substring(i, j + 1);
         }
-
         String sLeft = backwardResult(path, s, i, k - 1);
         String sRight = backwardResult(path, s, k, j);
         return sLeft + " " + sRight;
     }
+
 }
